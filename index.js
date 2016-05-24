6dd8e48c7b286eaa0eec965b501490c2a1c285b5
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2016-05-24 10:17:54 -0700

    Update to handle a list of events to listen for. Will only listen for the first one.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 'use strict';
 
 module.exports = function listenOnce(node, type, callback) {
-    var listener = function(e) {
-        node.removeEventListener(type, listener);
-        return callback(e);
-    };
+    var finished = false,
+        listener = function(e) {
+            if(type instanceof Array) {
+                type.forEach(function(typeInstance) {
+                    node.removeEventListener(type, listener);
+                });
+            } else {
+                node.removeEventListener(type, listener);
+            }
 
-    node.addEventListener(type, listener, false);
+            if(!finished) {
+                finished = true;
+                return callback(e);
+            }
+        };
+    if(type instanceof Array) {
+        type.forEach(function(typeInstance) {
+            node.addEventListener(typeInstance, listener, false);
+        });
+    } else {
+        node.addEventListener(type, listener, false);
+    }
 }
